3e351afea94e52ea030936191233a74d86463296
tar04 <taras200195@gmail.com>
2022-02-10 18:55:27 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-
 import App from './App';
 import './index.css';
 import store from "./store/store";
