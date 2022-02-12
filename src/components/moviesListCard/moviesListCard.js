10d3ac6007943f787d84c1b00099dc2d68a68316
tar04 <taras200195@gmail.com>
2022-02-12 23:53:24 +0200

    added genre change colors

--- src/components/moviesListCard/moviesListCard.js
+++ src/components/moviesListCard/moviesListCard.js
@@ -8,7 +8,7 @@ const MoviesListCard = ({movie:{id,title, poster_path, vote_average, overview}})
 
     return (
         <Link className={"movie"} data-title={overview} to={id.toString()}>
-            <img src={'https://image.tmdb.org/t/p/w500' + poster_path} alt={title}/>
+            <img src={poster_path ? ('https://image.tmdb.org/t/p/w500' + poster_path) : require('./default_image.jpg')} alt={title}/>
             <div className={"title"}>
                 <h3>{title}</h3>
             <div className= {vote_average<=5 ?"ratings_low":"ratings_high"} >{vote_average}</div>
