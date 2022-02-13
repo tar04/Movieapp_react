06fed001fdcb9a171eb897ca184fdd5a44998e07
tar04 <taras200195@gmail.com>
2022-02-13 20:55:55 +0200

    added theme change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-import App from './App';
-import './index.css';
+import App from "./App";
 import store from "./store/store";
+import "./index.css";
 
 
 ReactDOM.render(
