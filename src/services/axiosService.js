c6d8d54112fe07ee6647c5414bdba2344256d2ff
tar04 <taras200195@gmail.com>
2022-08-17 01:23:00 +0300

    moved token

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -5,7 +5,7 @@ import baseURL from "../config/urls";
 export const axiosService = axios.create({
     baseURL,
     headers: {
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZjAwZGM2YWNmNTFlNjkwZWE5ZDIxODRkNDFhODdlMSIsInN1YiI6IjYyMDUyNjFhZTMyM2YzMDBkMDJmMDJmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u-OMTFrvLMMT20tgGnLCqO_G7iYJkDWRBKcmht9mp-I"
+        Authorization: process.env.REACT_APP_BEARER_TOKEN
     }
 });
 
